a1f3ff8da2a1137057af2e2c062353ff61c551b6
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2019-07-07 22:27:59 +0200

    Add sketch of Keyword formatting.

--- format/WhitespacesAroundKeywords.go
+++ format/WhitespacesAroundKeywords.go
@@ -10,5 +10,25 @@ type WsAroundKeywords struct {
 	IndentAfter  bool
 }
 
+// Format method formats SQL keywords and whitespaces around them.
 func (w WsAroundKeywords) Format(script *read.Script) {
+	newWords := make([]string, 0, len(script.Words))
+
+	for wId, w := range script.Words {
+		f := (*script.Flags)[wId]
+		if f.IsMainKeyword && !f.IsComment {
+			newWord, ok := w.formatKeyword(w, f)
+			if ok {
+				newWords = append(newWords, newWord)
+			}
+			continue
+		}
+		newWords = append(newWords, w)
+	}
+
+	// Script update
+}
+
+func (w WsAroundKeywords) formatKeyword(w string, flags read.ScriptFlags) (string, bool) {
+	// TODO
 }
